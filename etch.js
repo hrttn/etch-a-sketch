5f3a36b757fe1874302609719893d6ad78f48b21
hrttn <hrettien@gmail.com>
2015-08-14 19:24:42 +0100

    Created the grid 16*16

--- etch.js
+++ etch.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-	for(var i = 0; i < 16*16; i++){
-		$(".grid").append("<div class='square'></div>");
+	for(var i = 0; i < 16; i++){
+		$(".grid").append("<div id='line" + i + "' class='line'></line>");
+		
+		for(var j=0; j < 16; j++){
+			$("#line" + i).append("<div id='square" + i + "_" + j + "' class='square'>"+ i + "-" + j + "</div>");
+		}
 	}
-
 });
